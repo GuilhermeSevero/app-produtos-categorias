97dbf3a17eefbc8a4867674a6e16f58cb9dfcdec
GuiSevero <guilherme@lojaspompeia.com.br>
2020-04-05 23:08:59 -0300

    Implementado Component de Form com Submit

---
+++ src/components/SubmitForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-form
+    class="q-gutter-md col"
+    style="max-width: 500px"
+    @submit="$emit('submit')"
+    @reset="$emit('reset')"
+  >
+    <slot />
+
+    <q-btn-group>
+      <q-btn
+        label="Salvar"
+        type="submit"
+        color="primary"
+        icon="save"
+      />
+      <q-btn
+        label="limpar"
+        type="reset"
+        color="primary"
+        icon="clear"
+        flat
+      />
+    </q-btn-group>
+  </q-form>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+
+  methods: {
+  }
+}
+</script>
