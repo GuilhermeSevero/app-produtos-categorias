6751474dfbab63ce63ad09bee2a0f636cfb669bd
GuiSevero <guilherme@lojaspompeia.com.br>
2020-04-05 21:37:21 -0300

    Pagina de Listagem das Categorias

---
+++ src/views/categorias/CategoriaLista.vue
@@ -0,0 +1,62 @@
+<template>
+  <api-table
+    ref="tbCategorias"
+    title="Categorias"
+    row-key="id_categoria_produto_planejamento"
+    :service="service"
+    :columns="columns"
+    selection="single"
+  />
+</template>
+
+<script>
+import ApiTable from '../../components/ApiTable'
+
+import categoriaService from '../../services/categoria'
+
+export default {
+  name: 'PgListaCategorias',
+
+  components: {
+    ApiTable
+  },
+
+  data() {
+    return {
+      columns: [
+        {
+          name: 'id_categoria_produto_planejamento',
+          required: true,
+          label: 'Código',
+          style: 'width: 10ch',
+          field: 'id_categoria_produto_planejamento',
+          sortable: true
+        },
+
+        {
+          name: 'nome_categoria',
+          required: true,
+          label: 'Descrição',
+          align: 'left',
+          field: 'nome_categoria',
+          sortable: true
+        }
+      ]
+    }
+  },
+
+  computed: {
+    service() {
+      return categoriaService
+    }
+  },
+
+  mounted() {
+    this.$refs.tbCategorias.refresh()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
